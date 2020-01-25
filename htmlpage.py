9ca67c2a768799cb6809fe91b93c7efc56b9d271
Andrea Moro nfs <andrea@andreamoro.net>
2020-01-25 05:09:34 +0000

    added link

--- htmlpage.py
+++ htmlpage.py
@@ -67,7 +67,8 @@ class htmlPage:
         tempstr += "(<a href='http://www.gutenberg.org/wiki/Gutenberg:The_Project_Gutenberg_License'>license</a>)</p>\n"
         tempstr += "<p>By <a href='http://andreamoro.net'>Andrea Moro</a>, Vanderbilt University, "
         tempstr += "<a href='mailto:andrea@andreamoro.net'>andrea at andreamoro dot net </a>"
-        tempstr += " - <a href='https://github.com/andreamoro-git/joyceconcordance'>Github code</a></p>\n</div>\n"
+        tempstr += " - <a href='https://github.com/andreamoro-git/joyceconcordance'>Github code</a></p>\n"
+        tempstr += "<a href='https://litconcordance.andreamoro.net''>Other concordances</a></p>\n</div>\n"
         tempstr +=  ("</body>\n")
         tempstr +=  ("</html>\n")
         return tempstr
